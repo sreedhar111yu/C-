77bbb15460fe3a7a769f3a554099b51b5b667382
sreedhar111yu <sreedharv36@gmail.com>
2024-02-28 14:25:55 +0530

    Better solution maxsub arr

---
+++ Phase2/BETTER maxSUBarr.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    int maxSum=arr[0];
+    for(int i=0;i<size;i++){
+        int sum=0;
+        for(int j=i;j<size;j++){
+            sum+=arr[j];
+        maxSum=max(maxSum,sum);
+        }
+    }
+    cout<<maxSum;
+}
