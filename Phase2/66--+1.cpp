91dadbfa29e28720950ad3d4237220178b695362
sreedhar111yu <sreedharv36@gmail.com>
2024-03-13 14:53:56 +0530

    second test case passeed pending 3 one

--- Phase2/66--+1.cpp
+++ Phase2/66--+1.cpp
@@ -1,19 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int size;
-    cin>>size;
-    int arr[size];
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-    
-         if(arr[size-1]<=9){
-         arr[size-1]++;
-         
-    }
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
 
+int main() {
+    int size;
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
     }
+
+    for (int i = size - 1; i >= 0; i--) {
+        if (arr[i] < 9) {
+            arr[i]++;
+            break;  
+        } else {
+            arr[i] = 0;
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
