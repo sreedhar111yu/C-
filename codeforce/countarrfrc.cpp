c3bebe0287076b7da821fec885382f827fbc72ca
sreedhar111yu <sreedharv36@gmail.com>
2024-03-18 19:41:28 +0530

    count frequeancy of array

---
+++ codeforce/countarrfrc.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    Unordered_map<int,int>mp
+    for(int i=0;i<size;i++){
+        mp[arr[i]]++;
+    }
+    for (auto& pair : mp) {
+        cout << pair.first << " occurs " << pair.second << " times" << endl;
+    }
+    return 0;
+    
+    }
