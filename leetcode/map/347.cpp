493c3afc6e31d3ccbc5513ef451c478402c09224
sreedhar111yu <sreedharv36@gmail.com>
2024-03-19 18:59:22 +0530

    leetcode

---
+++ leetcode/map/347.cpp
@@ -0,0 +1,25 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         vector<int>ans;
+//         int size=nums.size();
+
+//         for(int i=0;i<size;i++){
+//             mp[nums[i]]++;
+//         }
+
+//         priority_queue<pair<int,int>>pp;
+//         for(auto ele:mp){
+//             pp.push({ele.second,ele.first});
+//         }
+//         while(k!=0){
+//             int val=pp.top().second;
+//             ans.push_back(val);
+//             pp.pop();
+//             k--;
+//         }
+//         return ans;
+        
+//     }
+// };
