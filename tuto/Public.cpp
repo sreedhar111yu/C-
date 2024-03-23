f840c1fc58b3ed55471538991dea849871819e8e
sreedhar111yu <sreedharv36@gmail.com>
2024-03-23 13:53:15 +0530

    public classes in cpp tuto

---
+++ tuto/Public.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class student{
+    public:
+
+    string name;
+    int age;
+
+    
+
+};
+
+int main(){
+    student O;
+    O.name="sreedhar";
+    O.age=20;
+    cout<<O.name;
+    cout<<O.age;
+}
