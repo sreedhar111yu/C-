1690cc2c4efc825fc474854429669822a4cfb1f1
sreedhar111yu <sreedharv36@gmail.com>
2024-03-28 19:22:13 +0530

    optimal solution

--- Mr Cooper/majority element.cpp
+++ Mr Cooper/majority element.cpp
@@ -1,1 +1,38 @@
-//OPTIMAL Majority of element in arr
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorityele(vector<int> &arr){
+    int size = arr.size();
+    int N = arr[0];
+    int count = 1;
+
+    for(int i = 1; i < size; i++) { // Start from index 1
+        if(arr[i] == N) {
+            count++;
+        } else {
+            count--;
+        }
+        if(count == 0) {
+            N = arr[i];
+            count = 1; // Reset count
+        }
+    }
+    return N;
+}
+
+int main() {
+    int size;
+    cin >> size;
+    
+    vector<int> arr(size);
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    int res = majorityele(arr);
+    if(res != -1) {
+        cout << "Majority element is: " << res << endl;
+    } else {
+        cout << "No majority element found." << endl;
+    }
+    return 0;
+}
