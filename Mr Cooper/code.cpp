06346a6f4ce77860f20e8ab12b112902230472b9
sreedhar111yu <sreedharv36@gmail.com>
2024-03-28 18:19:11 +0530

    28.3

---
+++ Mr Cooper/code.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int bin(int k, int n,int x){
+    
+    for(int i=1;i<n;i++){
+        if(i==x||(i==x+1 || x<=n)){
+            
+        }
+    }
+
+}
