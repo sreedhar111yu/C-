d5b7f23b49afeec1f1e066b76aa3587cceafb6cc
sreedhar111yu <sreedharv36@gmail.com>
2024-06-06 18:05:43 +0530

    A character is an alphabet or not

---
+++ Math/A character is an alphabet or not.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(!isalpha(ch)){
+        cout<<"not alpha";
+    }
+    else{
+        cout<<"alpha";
+    }
+}
