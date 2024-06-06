b04fbbabfa471fa467e76d90642de96620d3e447
sreedhar111yu <sreedharv36@gmail.com>
2024-06-06 18:00:11 +0530

    A character is a vowel or consonant

---
+++ Math/A character is a vowel or consonant.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        cout<<"vowels";
+    }
+    else{
+        cout<<"consonant";
+    }
+    return 0;
+    
+}
