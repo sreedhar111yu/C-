7994c8fba6da3186d406c44a85c927866cadb275
sreedhar111yu <sreedharv36@gmail.com>
2024-06-10 14:32:53 +0530

    10

---
+++ PHSAE-3/high.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    int count =0;
+    for(int i=0;i<size;i++){
+        sort(arr.begin(),arr.end())
+
+    }
+}
