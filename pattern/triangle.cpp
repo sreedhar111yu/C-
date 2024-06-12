953ee0bff260c8432eed98b5cfb23e2c8116590e
sreedhar111yu <sreedharv36@gmail.com>
2024-06-12 10:17:51 +0530

    triangle

---
+++ pattern/triangle.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int noOfRow=5;
+    for(int row=1;row<=noOfRow;row++){
+        for(int col=1;col<=row;col++){
+            cout<<("* ");
+        }
+        cout<<("\n");
+    }
+}
