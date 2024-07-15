b4ceaa1e3237215ccd78eb9d06258d15f60e097a
sreedhar111yu <sreedharv36@gmail.com>
2024-07-15 20:15:30 +0530

    program to Check Whether a Number is Positive or Negative

---
+++ ACC/simple program.cpp/2.cpp
@@ -0,0 +1,14 @@
+// program to Check Whether a Number is Positive or Negative
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    if(num<0){
+        cout<<"-ive";
+    }
+    else{
+        cout<<"+ive";
+    }
+
+}
