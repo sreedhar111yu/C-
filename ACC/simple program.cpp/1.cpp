7d90ee89737fb60e515730d9b9829196df0c5064
sreedhar111yu <sreedharv36@gmail.com>
2024-07-15 20:09:43 +0530

    1 even

---
+++ ACC/simple program.cpp/1.cpp
@@ -0,0 +1,14 @@
+//C++ Program to Check Whether a Number is Even
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+
+    if(num%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"Odd";
+    }
+}
---
+++ console/Exp/Account.h
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class account{
+
+    string name;
+    string email;
+    string password;
+    int Accno;
+    double Bal;
+    
+};
