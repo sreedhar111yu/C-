cf89bc62bf5f95960ba18d692804c110d3414d9f
sreedhar111yu <sreedharv36@gmail.com>
2024-07-16 21:14:05 +0530

    pyr

---
+++ Zoho/pattern/12.cpp
@@ -0,0 +1,27 @@
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int size;
+    cin>>size;
+
+    for(int i=0;i<size;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<2*size-(2*i+1);k++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
