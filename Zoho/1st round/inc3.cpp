118a9395eef773ecb984481aa300e0c72d10e306
sreedhar111yu <sreedharv36@gmail.com>
2024-07-17 20:08:26 +0530

    inc

---
+++ Zoho/1st round/inc3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=3;
+    int b=2;
+
+    //int c=a++ * b;  //6
+    int d=++a * b;//8
+
+  //  cout<<c<<endl; 
+    cout<<d<<endl;
+    cout<<a;  //a=4 preinc 1+a=4 
+}
