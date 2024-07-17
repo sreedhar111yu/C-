d258ad396949b45c485d2a0173fdb3314c1fa247
sreedhar111yu <sreedharv36@gmail.com>
2024-07-17 20:43:27 +0530

    inc

---
+++ Zoho/1st round/inc4.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int i=2,j=3;
+    int k= --i + j;
+    int l=++i+j;
+
+    cout<<i<<endl<<j<<endl<<k<<endl<<l;
+}
---
+++ Zoho/1st round/inc5.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int m=2,n=3;
+   int o=m--+n;
+   int p=m++ + n;
+
+   cout<<m<<endl<<n<<endl<<o<<endl<<p;
+}
