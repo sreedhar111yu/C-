a992c0030713b0dc151c3cab5f087ce76e895e26
sreedhar111yu <sreedharv36@gmail.com>
2024-07-20 09:15:09 +0530

    inc

---
+++ Zoho/1st round/inc10.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=10;
+    int b=10;
+
+    if((a++==10) || (b++==10)){
+        cout<<(a+" "+b);
+
+    }
+    else{
+        cout<<"no";
+    }
+}
