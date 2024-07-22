03fd546643ba90262c0b8cc3782b36fc2a26f372
sreedhar111yu <sreedharv36@gmail.com>
2024-07-22 13:49:58 +0530

    b

--- Zoho/pattern/23.cpp
+++ Zoho/pattern/23.cpp
@@ -1,27 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
     int size;
     cin>>size;
 
     for(int i=0;i<size;i++){
-        for(int j=0;j<i;j++){
+        for(int j=0;j<size-i-1;j++){
             cout<<"*";
         }
-        for(int k=0;k<2*size-(2*i+1);k++){
+        for(int k=0;k<2*i+1;k++){
             cout<<" ";
         }
-         for(int j=0;j<i;j++){
+        for(int j=0;j<size-i-1;j++){
             cout<<"*";
         }
         cout<<endl;
     }
 }
 
-
-// *       *
-// **     **
-// ***   ***
 // **** ****
+// ***   ***
+// **     **
+// *       *
