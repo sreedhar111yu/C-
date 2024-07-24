1f8a3632bfc5dc27951d85fcd0e19609c59ee9bc
sreedhar111yu <sreedharv36@gmail.com>
2024-07-24 11:36:51 +0530

    mat

--- Zoho/matrix/9.cpp
+++ Zoho/matrix/9.cpp
@@ -1,8 +1,28 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// bool upper(int arr[size][size]){
-//     for(int i=1;i<size;i++){
+#include<bits/stdc++.h>
+using namespace std;
+#define N 4
 
-//     }
+bool lower(int mat[N][N]){
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            if(mat[i][j]!=0)
+            return false;
+        }
+    }
+    return true;
+}
 
-// };
+int main(){
+    int mat[N][N]={
+         {1, 0, 0, 0},
+        {1, 4, 0, 0},
+        {4, 6, 2, 0},
+        {0, 4, 7, 6}
+    };
+    if(lower(mat)){
+        cout<<"lower triangular matrix";
+    }
+    else{
+        cout<<"not lower triangular matrix ";
+    }
+}
